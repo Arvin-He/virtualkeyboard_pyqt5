0d32ba1a45eab91a1e643547e7dbb2e4004feb70
Arvin-He <510205617@qq.com>
2017-08-07 17:54:05 +0800

    添加按钮长按循环执行命令功能

--- virtualkeyboard/ctrlpanel.py
+++ virtualkeyboard/ctrlpanel.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 import os
+import time
 import functools
 from PyQt5 import uic
 from PyQt5 import QtWidgets
@@ -22,13 +23,46 @@ class ControlPanel(QtWidgets.QWidget):
         self.setFixedSize(self.ctrlpanelWidth, self.ctrlpanelHeight)
         for attr in dir(self.ui):
             obj = getattr(self.ui, attr)
-            if isinstance(obj, QtWidgets.QPushButton) or isinstance(obj, QtWidgets.QToolButton):
-                obj.clicked.connect(functools.partial(self.on_clicked, obj))
+            if isinstance(obj, QtWidgets.QPushButton) or \
+               isinstance(obj, QtWidgets.QToolButton):
+                obj.setAutoRepeat(True)
+                obj._repeate = False
+                obj.clicked.connect(
+                    functools.partial(self.on_handleClicked, obj))
+
+    def on_handleClicked(self, btn):
+        if btn.isDown():
+            if btn._repeate is False:
+                btn._repeate = True
+                btn.setAutoRepeatInterval(50)
+            else:
+                self.on_pressed(btn)
+        elif btn._repeate is True:
+            btn._repeate = False
+            self.on_released(btn)
+        else:
+            self.on_clicked(btn)
 
     def on_clicked(self, btn):
         try:
-            if btn.property("btn_cmd") is not None:
-                punggol_exec(btn.property("btn_cmd"))
+            if btn.property("clicked_cmd") is not None:
+                punggol_exec(btn.property("clicked_cmd"))
+        except Exception as e:
+            print(e)
+            return
+
+    def on_pressed(self, btn):
+        try:
+            if btn.property("pressed_cmd") is not None:
+                punggol_exec(btn.property("pressed_cmd"))
+        except Exception as e:
+            print(e)
+            return
+    
+    def on_released(self, btn):
+        try:
+            if btn.property("released_cmd") is not None:
+                punggol_exec(btn.property("released_cmd"))
         except Exception as e:
             print(e)
             return
